2dc2e732b44798a40ce4475d13f71d0265a94efa
Luzhanskiy7 <107493221+Luzhanskiy7@users.noreply.github.com>
2023-10-08 15:35:31 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -19,20 +19,4 @@ def test_get_product(client):
 if __name__ == '__main__':
     pytest.main()
 
-In this example, we've defined two simple unit tests using the pytest framework. We create a fixture called client to get a test client for the Flask application, which allows us to send HTTP requests and receive responses.
 
-The test_get_products and test_get_product functions are examples of test cases for the respective routes. You should add more tests for other routes and functionalities in a similar manner.
-
-To run the tests, you can use the following command in the terminal:
-
-bash
-
-pytest test_app.py
-
-Make sure you have pytest installed. If not, you can install it using pip:
-
-bash
-
-pip install pytest
-
-Remember to adjust and expand the tests based on the specific behavior and requirements of your application.
